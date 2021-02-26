feda6d6ba36a869d856e18fc0561d668b99edbbe
Omar Branez <omar.branez@gmail.com>
2021-02-26 14:23:38 -0500

    commit tree 45ed19ad5c8afde01c02809eeca6187e9b3943b8

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
 
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip = 0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        #meals.all.max_by {|meal| meal.tip}
+        best_tipped_meal = meals.max {|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+        #spaceship keeps checking to see which one is -1 (less than), 0 (equal), or 1 (greater than) and only return the 1 (the GREATEST)
+        best_tipped_meal.customer
+        #and returns the customer that meal belongs to
+    end
+    
 end
